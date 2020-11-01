eb88a629aca94adfd1e8e4648ee9408f397b83b6
vkozhinov <vkozhinov@voximplant.com>
2020-11-01 23:38:28 +0300

    initial commit, add files for docker, add script for init project

---
+++ init_project.sh
@@ -0,0 +1,9 @@
+cp .env.example .env
+echo "127.0.0.1 pay_task.local" | sudo tee --append /etc/hosts > /dev/null
+
+cd docker
+cp .env.example .env
+docker-compose up -d
+
+docker exec -ti pay_task_php bash
+composer install
