c96fba19bc7faa9d8592a235a4cc2a6ff97e05b9
vkozhinov <vkozhinov@voximplant.com>
2020-11-02 21:36:42 +0300

    auth: correct templates, add signup template, add model

---
+++ models/SignupForm.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace app\models;
+
+
+class SignupForm
+{
+    public string $username;
+    public string $email;
+    public string $password;
+    public string $confirmPassword;
+
+}
--- views/site/signup.php
+++ views/site/signup.php
@@ -16,7 +16,7 @@ $this->params['breadcrumbs'][] = $this->title;
     <p>Please fill out the following fields to login:</p>
 
     <?php $form = ActiveForm::begin([
-        'id' => 'login-form',
+        'id' => 'signup-form',
         'layout' => 'horizontal',
         'fieldConfig' => [
             'template' => "{label}\n<div class=\"col-lg-3\">{input}</div>\n<div class=\"col-lg-8\">{error}</div>",
@@ -28,21 +28,19 @@ $this->params['breadcrumbs'][] = $this->title;
 
         <?= $form->field($model, 'password')->passwordInput() ?>
 
-        <?= $form->field($model, 'rememberMe')->checkbox([
-            'template' => "<div class=\"col-lg-offset-1 col-lg-3\">{input} {label}</div>\n<div class=\"col-lg-8\">{error}</div>",
-        ]) ?>
+        <?= $form->field($model, 'confirmPassword')->passwordInput() ?>
 
         <div class="form-group">
             <div class="col-lg-offset-1 col-lg-11">
-                <?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
+                <?= Html::submitButton('Signup', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
             </div>
         </div>
 
     <?php ActiveForm::end(); ?>
 
     <div class="form-group">
-        <p>Don't have an account?
-            <?= Html::a('Sign up', 'site/signup', ['class' => 'profile-link']) ?>
+        <p>Already have an account?
+            <?= Html::a('Sign up', 'site/login', ['class' => 'profile-link']) ?>
         </p>
     </div>
 </div>
