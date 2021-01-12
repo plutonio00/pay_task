b64a3568615597210c1b6850dc02520570151590
vkozhinov <vkozhinov@voximplant.com>
2021-01-12 21:41:02 +0300

    fixtures: fix bug (now every user get at least two wallets)

--- tests/unit/fixtures/data/wallet.php
+++ tests/unit/fixtures/data/wallet.php
@@ -13,8 +13,17 @@ for($i = 0; $i < Constants::WALLET_COUNT; $i++) {
     $updatedAt = $faker->dateTimeBetween($createdAt, '+30 days')
         ->format(Constants::DATE_TIME_FORMAT);
 
+    /**
+     * For correct creation of transfers, each user must have at least two accounts.
+     * Otherwise, a situation may occur when the user is selected,
+     * but he has no accounts to transfer, or the user is selected twice,
+     * but he cannot transfer money to his other account.
+     */
+    $userId = $i < Constants::DOUBLE_USER_COUNT ?
+        $userIds[$i % Constants::USER_COUNT] : $faker->randomElement($userIds);
+
     $wallets[] = [
-        'id_user' => $faker->randomElement($userIds),
+        'id_user' => $userId,
         'title' => 'wallet_' . $i,
         'amount' => $faker->randomFloat(2, 2000, 5000),
         'created_at' => $createdAt->format(Constants::DATE_TIME_FORMAT),
