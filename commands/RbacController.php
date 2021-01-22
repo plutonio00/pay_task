660164973852b20863da6901c06907190ac64962
vkozhinov <vkozhinov@voximplant.com>
2021-01-22 19:37:35 +0300

    rbac: add console command for rbac

--- commands/RbacController.php
+++ commands/RbacController.php
@@ -2,16 +2,19 @@
 
 namespace app\commands;
 
+use app\models\User;
+use Exception;
 use Yii;
 use yii\console\Controller;
+use yii\console\ExitCode;
 
-class RBACController extends Controller
+class RbacController extends Controller
 {
     /**
      * @throws yii\base\Exception
-     * @throws \Exception
+     * @throws Exception
      */
-    public function actionInitRoles(): void
+    public function actionInitRoles(): int
     {
         $auth = Yii::$app->authManager;
         $user = $auth->createRole('user');
@@ -19,5 +22,31 @@ class RBACController extends Controller
         $admin = $auth->createRole('admin');
         $auth->add($admin);
         $auth->addChild($admin, $user);
+
+        return ExitCode::OK;
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function actionAssignRolesForFixtureUsers(): int
+    {
+        /** @var User[] $users */
+        $users = User::find()->all();
+        $auth = Yii::$app->authManager;
+        $userRole = $auth->getRole('user');
+        $adminRole = $auth->getRole('admin');
+        $adminIndexes = array_rand($users, 2);
+
+        foreach ($adminIndexes as $adminIndex) {
+            $auth->assign($adminRole, $users[$adminIndex]->id);
+            unset($users[$adminIndex]);
+        }
+
+        foreach ($users as $user) {
+            $auth->assign($userRole, $user['id']);
+        }
+
+        return ExitCode::OK;
     }
 }
